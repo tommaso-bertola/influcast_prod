408afd17bd0cfa62a4e35ebf70faccb18788aef5
Tommaso Bertola <tommaso.bertola+influcastVm@phd.unipd.it>
2024-11-20 16:42:21 +0100

    less verbose messaging when pulling form git

--- scripts/download_new_data.sh
+++ scripts/download_new_data.sh
@@ -16,7 +16,7 @@ notify "Pulling data from Influcast repository" $host
 # echo "git pull"
 file="/home/ubuntu/influcast_prod/joblog/job_$(date '+%Y-%m-%d_%H-%M-%S')_git_pull_log_$host.txt"
 
-git checkout main  >> file
-gh repo sync tommaso-bertola/Influcast -b main >> file
-git pull >> file
+git checkout main >> $file 2>&1
+gh repo sync tommaso-bertola/Influcast -b main >> $file 2>&1
+git pull >> $file 2>&1
 cd /home/ubuntu/influcast_prod
