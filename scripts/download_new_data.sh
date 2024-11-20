f08bc55183c1ce7c369a7dff3c29374da14d4c69
Tommaso Bertola <tommaso.bertola+influcastVm@phd.unipd.it>
2024-11-20 16:34:16 +0100

    adding logging to update Influcast repo

--- scripts/download_new_data.sh
+++ scripts/download_new_data.sh
@@ -14,7 +14,9 @@ notify "Pulling data from Influcast repository" $host
 # echo "git checkout main"
 # echo "gh repo sync tommaso-bertola/Influcast -b main"
 # echo "git pull"
-git checkout main
-gh repo sync tommaso-bertola/Influcast -b main
-git pull
+file="/home/ubuntu/influcast_prod/joblog/job_$(date '+%Y-%m-%d_%H-%M-%S')_git_pull_log_$host.txt"
+
+git checkout main  >> file
+gh repo sync tommaso-bertola/Influcast -b main >> file
+git pull >> file
 cd /home/ubuntu/influcast_prod
