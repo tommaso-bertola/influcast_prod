078e904076aa2d6fb16f9e802fc60406bc7fb9d8
Tommaso Bertola <tommaso.bertola+influcastVm@phd.unipd.it>
2025-09-25 09:36:02 +0200

    set up for complete retrospective analysis

--- source/get_retrospective_data.R
+++ source/get_retrospective_data.R
@@ -1,16 +1,50 @@
+# source file of weeks to compute again
 path <- "/home/ubuntu/influcast_prod/retrospective/missing_rounds_comunipd-mobnetSI2R_working_copy.csv"
-
 missing_rounds <- read.csv(path, header = FALSE)
 colnames(missing_rounds) <- c("name", "output_file", "target", "url")
 
+# get first row info
 first_row <- missing_rounds[1, ]
-target <- substr(first_row$target, nchar(first_row$target), nchar(first_row$target))
-output_path <- paste0("/home/ubuntu/Influcast/sorveglianza/ILI+_FLU/2024-2025/latest/italia-latest-ILI+_FLU_", target, ".csv")
 
+# get the target
+target <- substr(first_row$target, nchar(first_row$target), nchar(first_row$target))
+
+# recover regional latest data if signal=I
+# and determine the output path
+if (target == "I") {
+    target <- "ILI"
+    output_path <- paste0("/home/ubuntu/Influcast/sorveglianza/ILI/2024-2025/latest/italia-latest-ILI.csv")
+    regions <- c(
+        "abruzzo", "basilicata",
+        "calabria", "campania",
+        "emilia_romagna", "friuli_venezia_giulia",
+        "italia", "lazio",
+        "liguria", "lombardia",
+        "marche", "molise",
+        "pa_bolzano", "pa_trento",
+        "piemonte", "puglia",
+        "sardegna", "sicilia",
+        "toscana", "umbria",
+        "valle_d_aosta", "veneto",
+    )
+    for (region in regions) {
+        region_path <- paste0("/home/ubuntu/Influcast/sorveglianza/ILI/2024-2025/latest/", region)
+        region_url <- gsub("/italia-", paste0("/", region, "-"), first_row$url)
+        remote_df <- read.csv(region_url)
+        write.csv(remote_df, paste0(region_path, "-latest-ILI.csv"), row.names = FALSE, quote = FALSE)
+    }
+} else {
+    output_path <- paste0("/home/ubuntu/Influcast/sorveglianza/ILI+_FLU/2024-2025/latest/italia-latest-ILI+_FLU_", target, ".csv")
+}
+
+# download and write the "new" data
 remote_data <- read.csv(first_row$url)
 write.csv(remote_data, output_path, row.names = FALSE, quote = FALSE)
-signal_file <- "/home/ubuntu/influcast_prod/uploading_predictions/current_signal.txt"
 
+# update the signal file
+signal_file <- "/home/ubuntu/influcast_prod/uploading_predictions/current_signal.txt"
 writeLines(target, signal_file)
+
+# remove the first row
 colnames(missing_rounds) <- NULL
 write.csv(missing_rounds[-1, ], path, row.names = FALSE, quote = FALSE)
